25f445a235a312cfe5d2f53f05107f192657b10b
MITHUN NAIK <mithunnaik28c@gmail.com>
2026-07-26 15:07:59 +0530

    Create array_1.cpp

---
+++ Arrays/array_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int marks[5]={56,34,98,78,23};
+
+    cout<<marks[0]<<endl;
+    cout<<marks[2]<<endl;
+    cout<<marks[4]<<endl;
+    return 0;
+}
+
+//56
+//98
+//23

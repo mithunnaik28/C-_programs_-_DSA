7a8a5d60297d166999ab9f372be26df79481d166
MITHUN NAIK <mithunnaik28c@gmail.com>
2026-07-29 23:02:01 +0530

    Create loops_on_arrays.cpp

---
+++ Arrays/loops_on_arrays.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int marks[5]={56,34,98,78,23};
+    int size=5;
+    
+    //loops
+    for( int i=0; i<size; i++){
+        cout<< marks[i]<<endl;
+    }
+    return 0;
+}
+
+// 56
+// 34
+// 98
+// 78
+// 23

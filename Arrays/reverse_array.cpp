3a541eb0906fcab1892d876b583df35bc5684bf0
MITHUN NAIK <mithunnaik28c@gmail.com>
2026-08-08 23:48:36 +0530

    Create reverse_array.cpp

---
+++ Arrays/reverse_array.cpp
@@ -0,0 +1,29 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int N;
+    cin>>N;
+    vector<int> A(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >>A[i];
+    }
+    
+    reverse(A.begin(),A.end());
+    
+    for(int i=0; i<N;i++){
+        cout<<A[i]<<" ";
+    }
+    return 0;
+}
+
+// 4
+// 1 2 6 3 
+// 3 6 2 1 
